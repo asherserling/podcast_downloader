6f1b6fc8eb2c988e57eb9670bac6d7ce22cb1975
Asher Serling <asherserling@gmail.com>
2020-09-07 23:53:18 +0300

    choose interface version

---
+++ choose_interface.py
@@ -0,0 +1,27 @@
+import webbrowser
+import sys
+from threading import Timer
+
+from podcast_download.f_interface import app as flask_app
+from podcast_download.cli.app import PodcastApp
+
+
+def open_browser():
+    webbrowser.open_new('http://127.0.0.1:2000/')
+
+
+def run_flask_interface():
+    Timer(1, open_browser).start()
+    flask_app.run(port=2000)
+
+
+def run_cl_interface():
+    cli_app = PodcastApp()
+    cli_app.run()
+
+
+if __name__ == '__main__':
+    if sys.argv[-1] == 'flask':
+        run_flask_interface()
+    else:
+        run_cl_interface()
